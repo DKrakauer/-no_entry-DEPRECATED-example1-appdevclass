a7e1a0cf4b6fd1cc594d68801023a1859eee415a
David Krakauer <DKrakauer18@brophybroncos.org>
2017-09-20 10:06:44 -0700

    Initial Commit

---
+++ MyFirstView Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstView Project
+//
+//  Created by David Krakauer on 9/20/17.
+//  Copyright © 2017 David Krakauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
